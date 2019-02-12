67b6431f84b64ac0f834b40b724a30250808d07c
Marco Castro <marcodotcastro@gmail.com>
2019-02-12 12:45:40 +0000

    adicionar crud video no produtor

--- app/admin/produtors.rb
+++ app/admin/produtors.rb
@@ -1,7 +1,8 @@
 ActiveAdmin.register Produtor do
     menu priority: 1
 
-    permit_params :nome, :telefone, :email, :endereco, :video, :foto, :whatsapp, :cartao
+    permit_params :nome, :telefone, :email, :endereco, :foto, :whatsapp, :cartao,
+    videos_attributes: [:id, :nome, :descricao, :codigo, :_destroy]
     
     filter :nome
     
@@ -16,28 +17,31 @@ ActiveAdmin.register Produtor do
         column :cartao
         column :email
         column :endereco
-        column :video do |obj|
-            link_to "Assista" , "https://www.youtube.com/watch?v=#{obj.video}", target: "_blank"
-        end
         
         actions
     end
 
     form do |f|
         f.inputs do
-          f.input :nome
-          f.input :telefone
-          f.input :whatsapp
-          f.input :cartao
-          f.input :email
-          f.input :endereco
-          f.input :video
-          f.input :foto
-          actions
+            f.input :nome
+            f.input :telefone
+            f.input :whatsapp
+            f.input :cartao
+            f.input :email
+            f.input :endereco
+            f.input :foto
+            f.inputs do
+              f.has_many :videos, allow_destroy: true, new_record: true do |a|
+                a.input :nome
+                a.input :descricao
+                a.input :codigo
+              end
+            end
+           f.actions
         end
     end
   
-    show do
+    show title: proc{|p| "Produtor: " + p.nome }do
         attributes_table do 
             row :foto do |obj|
                 image_tag obj.foto, size: "300x200"
@@ -48,19 +52,28 @@ ActiveAdmin.register Produtor do
             row :cartao
             row :email
             row :endereco
-            row :video
             row :created_at
             row :updated_at
         end
-        table_for produtor.produtos do
-            column  :nome
-            column  :producao
-            column  :preco
-            column "" do |obj|
-                span link_to "Visualizar", admin_produto_path(obj)
-                span link_to "Editar", edit_admin_produto_path(obj)
+        panel "Produtos" do
+            table_for produtor.produtos do
+                column  :nome
+                column  :producao
+                column  :preco
+                column "" do |obj|
+                    span link_to "Visualizar", admin_produto_path(obj)
+                    span link_to "Editar", edit_admin_produto_path(obj)
+                end
+            end
+        end
+        panel "Vídeos" do
+            table_for produtor.videos do
+                column  :nome
+                column  :descricao
+                column :codigo do |obj|
+                    link_to "Assista" , "https://www.youtube.com/watch?v=#{obj.codigo}", target: "_blank"
+                end
             end
         end
     end
-
 end
--- app/models/produtor.rb
+++ app/models/produtor.rb
@@ -1,4 +1,6 @@
 class Produtor < ApplicationRecord
     has_many :produtos
     has_many :videos
+    
+    accepts_nested_attributes_for :videos, allow_destroy: true
 end
