248477599f2d70422ab822ae37fde5f27330eb57
Marco Castro <marcodotcastro@gmail.com>
2019-02-14 12:46:39 +0000

    [add] página principal dinâmica

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,9 @@ class HomeController < ApplicationController
   layout "home"
   
   def index
+    @produto = Produto.all.sample(1).first
+    @produtos = Produto.all.sample(3)
+    
+    @produtores = Produtor.all.sample(3)
   end
 end
