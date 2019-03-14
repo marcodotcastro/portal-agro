d90e48a4cae524fc57bd8e8fd629e53f9b95fc69
Marco Castro <marcodotcastro@gmail.com>
2019-03-14 11:25:37 -0300

    [r] mensagens html

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,9 @@ class HomeController < ApplicationController
   
   def index
     @produto = Produto.all.sample(1).first
+
     @produtos = Produto.all.sample(3)
-    
+    @servicos = Servico.all.sample(3)
     @produtores = Produtor.all.sample(3)
   end
 end
