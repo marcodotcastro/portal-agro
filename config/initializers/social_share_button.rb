a63ac215ef41dc12c1ea14f0b8664f064b2a80f5
Marco Castro <marcodotcastro@gmail.com>
2019-03-23 10:15:58 -0300

    [f] social share

--- config/initializers/social_share_button.rb
+++ config/initializers/social_share_button.rb
@@ -1,4 +1,4 @@
 SocialShareButton.configure do |config|
-  config.allow_sites = %w(whatsapp_web twitter google_bookmark )
+  config.allow_sites = %w(whatsapp_app twitter google_bookmark )
   # config.allow_sites = %w(whatsapp_web facebook twitter google_bookmark )
 end
