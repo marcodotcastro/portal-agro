87f86afb96a5fe0ec471d1eb00dedf45b8bc4670
Marco Castro <marcodotcastro@gmail.com>
2021-09-21 07:20:02 -0300

    build: heroku config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,4 +21,12 @@ Rails.application.configure do
   config.active_record.verbose_query_logs = true
   config.assets.debug = true
   config.assets.quiet = true
+
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
 end
