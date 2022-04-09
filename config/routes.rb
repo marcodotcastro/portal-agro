8974b0307a228f7cb5d695962f9b08a6bad601b8
Marco Castro <marcodotcastro@gmail.com>
2022-04-09 07:16:45 -0300

    feat(product): list

--- config/routes.rb
+++ config/routes.rb
@@ -123,6 +123,7 @@
 #           rails_direct_uploads POST       /rails/active_storage/direct_uploads(.:format)                                           active_storage/direct_uploads#create
 
 Rails.application.routes.draw do
+  get 'products/index'
   get 'home/index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
